42a9428893b64b539cdc43727b36753a511a78c4
klembela-machines <klembela@gmail.com>
2025-10-02 23:19:38 +0200

    day 7: Python input and formatted output

---
+++ user_profile.py
@@ -0,0 +1,19 @@
+# user_profile.py
+# mini project - day 7 task
+
+print("= = = User Profile creator = = = ")
+
+# Get user input
+name = input ("Enter your name : ")
+age = input("Enter your age: ")
+favorite_programming = input("Enter your favorite programming language: ")
+hobby = input("enter your hobby: ")
+
+# Print formmated summary
+print("Hello" + name + "!")
+print("\n" + "="* 40)
+print("you are: " + age + " years old.")
+print("your favorite programming is: " + favorite_programming + " and your hobby")
+print("=" * 40)
+
+
